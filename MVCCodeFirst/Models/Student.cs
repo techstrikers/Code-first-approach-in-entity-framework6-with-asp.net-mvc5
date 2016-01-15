5141b1bbeaeea2e15914a35dc16b884c58c51a5c
techstrikers <ajaysaptaputre@techstrikers.com>
2016-01-15 20:29:57 +0530

    CheckIn

---
+++ MVCCodeFirst/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCodeFirst.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string EmailId { get; set; }
+        public string Genter { get; set; }
+        [DataType(DataType.Date),DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}",ApplyFormatInEditMode = true)]
+        public DateTime? DOB { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ MVCCodeFirst/Models/StudentContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCCodeFirst.Models
+{
+    public class StudentContext: DbContext
+    {
+        public StudentContext()
+            : base("StudentDbContext")
+    {
+    }
+        public DbSet<Student> Students { get; set; }
+    }
+}
